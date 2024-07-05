b7fddd37dfe633d0018dae22ab50eee42a54ebf2
derRiesenOtter <robin.ender@icloud.com>
2024-07-05 12:03:32 +0200

    nvim cleaned up

--- nvim/lua/robin/plugins/slime.lua
+++ nvim/lua/robin/plugins/slime.lua
@@ -2,6 +2,6 @@ return {
 	"jpalardy/vim-slime",
 	config = function()
 		vim.cmd([[let g:slime_target = "tmux"]])
-		vim.cmd([[let g:slime_default_config = {"socket_name": "default", "target_pane": "1.1"}]])
+		vim.cmd([[let g:slime_default_config = {"socket_name": "default", "target_pane": "1.2"}]])
 	end,
 }
